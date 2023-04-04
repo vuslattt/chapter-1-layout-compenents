a45788dd91cb8831a3c86d283a8d040b65703a27
vuslattt <vuslatcoskun2001@gmail.com>
2023-04-04 14:10:55 +0300

    Initial Commit

---
+++ chapter 1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  chapter 1
+//
+//  Created by vuslat coşkun on 4.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chapter 1/chapter_1App.swift
@@ -0,0 +1,17 @@
+//
+//  chapter_1App.swift
+//  chapter 1
+//
+//  Created by vuslat coşkun on 4.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct chapter_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
